ac7a80230effb44e28857fc4c653294381acb934
Elior <heliosaian@gmail.com>
2026-07-03 17:02:56 +0300

    Refactor color handling: replace swatch elements with styled text spans, improve clipboard functionality, and simplify CSS styles.

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const MATCHES = [
+  "https://github.com/*",
+  "https://gitlab.com/*",
+  "https://gitee.com/*",
+  "https://bitbucket.org/*",
+  "https://dev.azure.com/*",
+  "https://*.github.dev/*",
+  "https://*.gitpod.io/*",
+];
